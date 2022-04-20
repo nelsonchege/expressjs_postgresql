8581cde9e50fcb509bf36671b6f66ac363fb5dc7
nelson chege <nelson3chege@gmail.com>
2022-04-20 14:46:04 +0300

    (update): hide postgres details

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 const Pool = require('pg').Pool;
 
 const pool= new Pool({
-    user:"postgres",
-    password:"wslpassword2",
-    database:"todo_database",
+    user:process.env.POSTGRES_USER,
+    password:process.env.POSTGRES_PASSWORD,
+    database:process.env.POSTGRES_DATABASE,
     host:"localhost",
     port:5432
 });
